8a2e0b9d065bdb3dc07fa234d0d120e31ef53e85
Paul <udegbuepaul43@gmail.com>
2026-02-03 03:50:58 +0100

    SettlementAcocunt 1 added

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const settlementAccounts = [
+{
+    id: 1,
+    bankName: "GTBank Plc",
+    accountName: "Akinsola Jegede Enterprises",
+    accountNumber: "0123456789",
+    currency: "NGN",
+    type: "Settlement",
+    isPrimary: true,
+    verified: true
+},
