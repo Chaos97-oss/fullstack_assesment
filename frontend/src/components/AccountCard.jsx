f82bfa306b1e7aa79072e24b8574b31e2c6311dc
Paul <udegbuepaul43@gmail.com>
2026-02-03 04:49:20 +0100

    fixing account card

---
+++ frontend/src/components/AccountCard.jsx
@@ -0,0 +1,12 @@
+const AccountCard = ({ account }) => {
+    return (
+        <div
+        style={{
+            border: "1px solid #ddd",
+            borderRadius: "10px",
+            padding: "20px",
+            marginBottom: "15px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }}
