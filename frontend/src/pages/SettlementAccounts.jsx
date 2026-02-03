93b6fbc8c30a3755c08aebcf3ec5a436b37b957f
Paul <udegbuepaul43@gmail.com>
2026-02-03 04:45:59 +0100

    Fixing

--- frontend/src/pages/SettlementAccounts.jsx
+++ frontend/src/pages/SettlementAccounts.jsx
@@ -1,3 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import AccountCard from "../components/AccountCard";
+
+const SettlementAccounts = () => {
+    const [accounts, setAccounts] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:5000/api/settlement-accounts")
+        .then(res => setAccounts(res.data.data))
+        .catch(err => console.error(err));
+    }, []);
